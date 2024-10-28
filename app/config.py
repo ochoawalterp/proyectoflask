ff09b81711b6df81e7f0c600d1b696f5d4bcd362
Walter Ochoa <wochoa56@gmail.com>
2024-10-27 19:39:02 -0600

    Proyecto con conexion a Base de datos

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123@localhost:5432/mi_flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Aquí puedes añadir más configuraciones
